390eb86a79282ab955a7cc95b73c6ff93bdfbc63
depthso <86912923+depthso@users.noreply.github.com>
2023-03-02 09:01:40 +0000

    Faster

--- AccJoin.py
+++ AccJoin.py
@@ -4,21 +4,8 @@ currentpath = str(pathlib.Path(__file__).parent.absolute())
 
 
 def startroblox(PlaceID: int, JobID: str, Cookie: str):
-    print("checking latest version of ROBLOX... ", end="")
-
     version = requests.get("http://setup.roblox.com/version.txt").content.decode("ascii")
     path = os.getenv("LOCALAPPDATA") + "\\Roblox\\Versions\\" + version
-
-    print("done! (" + version + ")")
-
-    if not os.path.exists(path):
-        print("updating ROBLOX, please wait... ", end="")
-
-        bootstrapper = requests.get("http://setup.roblox.com/RobloxPlayerLauncher.exe")
-        open(currentpath + "\\RobloxPlayerLauncher.exe", "wb").write(bootstrapper.content)
-        os.system('"' + currentpath + '\\RobloxPlayerLauncher.exe" -install')
-
-        print("done!")
 
     print("fetching CSRF token... ", end="")
 
@@ -51,7 +38,7 @@ def startroblox(PlaceID: int, JobID: str, Cookie: str):
     timestamp = '{0:.0f}'.format(round(time.time() * 1000))
     url = urllib.parse.quote(
         #"https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestPrivateGame&browserTrackerId=147062882894&placeId=142823291&accessCode=700a7d2c-2c91-48c3-86fd-a5e75d03569d&linkCode=77685428874983661291575613020671 browsertrackerid:147062882894 robloxLocale:en_us gameLocale:en_us channel: LaunchExp:InApp"
-        f'https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGame{"Job"}&browserTrackerId=147062882894&placeId={PlaceID}{"&gameId=" + JobID}&isPlayTogetherGame=false+browsertrackerid:147062882894+robloxLocale:en_us+gameLocale:en_us+channel:'
+        f'https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGame{"Job"}&browserTrackerId=147062882894&placeId={PlaceID}{"&gameId=" + JobID}&isPlayTogetherGame=false+browsertrackerid:147062882894+robloxLocale:en_us+gameLocale:en_us+channel:+LaunchExp:InApp'
     )
 
     os.startfile(f"roblox-player:1+launchmode:play+gameinfo:{ticket}+launchtime:{timestamp}+placelauncherurl:{url}")
